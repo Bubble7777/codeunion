7cbcc3eae41d3987c409fd720814cb0a309e28c2
DamirKrg <sdmmds93@gmail.com>
2021-10-06 01:00:46 +0600

    add work with block

---
+++ lib/src/screens/auth/bloc/log_in_bloc.dart
@@ -0,0 +1,42 @@
+import 'package:bloc/bloc.dart';
+import 'package:codeunion/src/common/models/tokens_model.dart';
+
+import 'package:dio/dio.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:meta/meta.dart';
+
+part 'log_in_event.dart';
+part 'log_in_state.dart';
+
+class LogInBloc extends Bloc<LogInEvent, LogInState> {
+  LogInBloc() : super(LogInInitial());
+  final Dio dio = Dio();
+  final Box tokensBox = Hive.box('tokens');
+
+  @override
+  Stream<LogInState> mapEventToState(LogInEvent event) async* {
+    if (event is LogInPressed) {
+      LogInLoading();
+      try {
+        Response response = await dio.post(
+          'http://api.codeunion.kz/api/v1/auth/login',
+          data: {
+            'email': event.email,
+            'password': event.password,
+          },
+        );
+        TokensModel tokensModel = TokensModel.fromJson(response.data['tokens']);
+
+        tokensBox.put('access', tokensModel.access);
+        tokensBox.put('refresh', tokensModel.refresh);
+        yield LogInLoaded();
+      } on DioError catch (e) {
+        yield LogInFailed(message: 'Неправильный логин или пароль');
+        throw e;
+      } catch (e) {
+        yield LogInFailed(message: 'Произошла ошибка');
+        throw e;
+      }
+    }
+  }
+}
